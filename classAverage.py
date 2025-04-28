490be34904fc3e6504660fd8a57859406005ff23
SXL1151 <sxl1151@g.coppellisd.com>
2025-04-28 09:33:07 -0500

    Create classAverage.py

---
+++ classAverage.py
@@ -0,0 +1,27 @@
+import streamlit as st
+class AvgInp:
+  def classAverageInpOL(j):
+    classAVG = 0
+    for i in range(1):
+    
+      classAVG = st.number_input("What is your class average %?",
+           placeholder="Must be a number",min_value=0.10, max_value=100.00,
+           key=f"cavgOL_{j}")
+    return classAVG
+  def classAverageInpHonors(j):
+    classAVG = 0
+    for i in range(1):
+      
+      classAVG = st.number_input("What is your class average %?",
+           placeholder="Must be a number",min_value=0.10, max_value=100.00,
+           key=f"cavgHon_{j}")
+    return classAVG
+  def classAverageInpAPIB(j):
+    classAVG = 0
+    for i in range(1):
+    
+      classAVG = st.number_input("What is your class average %?",
+         placeholder="Must be a number",min_value=0.10, max_value=100.00,
+         key=f"cavgAP_{j}")
+    return classAVG
+  
