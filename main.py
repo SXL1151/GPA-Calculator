38913fe19d8f558c84467e641efc703f6f0696e6
SXL1151 <sxl1151@g.coppellisd.com>
2025-04-29 12:45:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import streamlit as st
 import pdb
 import GPACop
 import classAverage
+import pandas as pd
 TotGPA = 0
+cNAMES = []
+cGPAS = []
+
+
 st.image(
     "https://media-hosting.imagekit.io/08168422c2f645f9/IMG_1657.png?Expires=1839961164&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=D51gbtOWwAKHNmzhT6r9HNI5K-VKbdti3-2kbUwdd2A649TiXaLzf6GmwO42h5L8ty1RMsmuEkfIxoADqIWfQVDg0XY0uICM~JgnPdXAA0gIqDZUuVYHXG2O3MWt9GoVGR58Sw71K7H3u3zZ5pEY7ezwOnoXcoLK4oSX1uS~EKvwALG-1K9VTUUBtdsmhFlp~TxkSIOh9TW6jC5FD6~XZodWv1uTvHU7OvVewGnrSrL56RlBtsa0CTaHjkq6tPcq-XYirpcVOUagFam2yWQy23IFAgSdH73tSvhrrkkv6v6qTkkVpt5ZK8qghM2hArQwqG8k~D6z-Kg95D0eIuUaaA__",
     width=20)
@@ -31,23 +36,38 @@ if ChoGPA == "Weighted GPA":
       st.text(f"You have {num_classes} classes")
       break
     except ValueError:
-      st.badge("Please enter an integer", color="yellow")
+      st.badge("Please enter an integer", color="red")
   for i in range(num_classes):
     cName = st.text_input("Class #" + str(i + 1) + ": What is the class name?)",
                           placeholder="optional", key=f"cname_{i}"
                           )
+    if cName == "":
+
+      cName = "Class #" + str(i+1)
+    else:
+      pass
+    
     cLevel = st.selectbox("What is your class level?",("On-Level", "Honors", "AP/IB"), key = f"clevel_{i}")
     
     
     if cLevel == "On-Level":
       #cAverage = classAverage.classAverageInp
-      TotGPA = TotGPA + GPACop.CoppellGPAScale.OLGPA(num_classes-(num_classes-i))
+      cGPA = GPACop.CoppellGPAScale.OLGPA(num_classes-(num_classes-i))
+      TotGPA = TotGPA + cGPA
+      cNAMES.append(cName)
+      cGPAS.append(str(cGPA))
       st.text("________________________________________________________________________________________________________________________________________________________________________________")
     elif cLevel == "Honors":
-      TotGPA = TotGPA + GPACop.CoppellGPAScale.HonorGPA(num_classes-(num_classes-i))
+      cGPA = GPACop.CoppellGPAScale.HonorGPA(num_classes-(num_classes-i))
+      TotGPA = TotGPA + cGPA
+      cNAMES.append(cName)
+      cGPAS.append(str(cGPA))
       st.text("________________________________________________________________________________________________________________________________________________________________________________")
     elif cLevel == "AP/IB":
-      TotGPA = TotGPA + GPACop.CoppellGPAScale.APIBGPA(num_classes-(num_classes-i))
+      cGPA = GPACop.CoppellGPAScale.APIBGPA(num_classes-(num_classes-i))
+      TotGPA = TotGPA + cGPA
+      cNAMES.append(cName)
+      cGPAS.append(str(cGPA))
       st.text("________________________________________________________________________________________________________________________________________________________________________________")
 elif ChoGPA == "Unweighted GPA":
   while True:
@@ -65,16 +85,43 @@ elif ChoGPA == "Unweighted GPA":
     except ValueError:
       st.badge("Please enter an integer", color="yellow")
   for i in range(num_classes):
+    
     cName = st.text_input("Class #" + str(i + 1) + ": What is the class name?)",
                           placeholder="optional", key=f"cname_{i}"
                           )
-    GPACop.CoppellGPAScale.UnweighedGPA(num_classes-(num_classes-i))
+    if cName == "":
+      
+      cName = "Class #" + str(i+1)
+    else:
+      pass
+    
+    cGPA = GPACop.CoppellGPAScale.UnweighedGPA(num_classes-(num_classes-i))
+    TotGPA = TotGPA + cGPA
+    cNAMES.append(cName)
+    cGPAS.append(str(cGPA))
+    #pdb.set_trace()
+    
+    
     st.text("________________________________________________________________________________________________________________________________________________________________________________")
       
 try:
   st.warning(f"Your total GPA is {TotGPA/num_classes}")
 except:
   st.badge("No classes to calculate GPA", color="red")
+
+finTable = {"Class Name": cNAMES, "Class GPA": cGPAS}
+cNAMES.append("TOTAL GPA")
+try:
+  cGPAS.append(str(TotGPA/num_classes))
+  booGo = True
+except ZeroDivisionError:
+  st.badge("No classes to build GPA Table", color="red")
+  booGo = False
+if booGo == False:
+  pass
+elif booGo == True:
+  st.table(finTable)
+#pdb.set_trace()
     
 
     
