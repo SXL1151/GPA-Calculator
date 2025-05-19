595f4762cbff75dd2e1a0372aabc85fb62976f7f
SXL1151 <sxl1151@g.coppellisd.com>
2025-05-19 11:38:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import streamlit as st
-import pdb
-import GPACop
-import classAverage
 import pandas as pd
+import GPACop
+
 TotGPA = 0
 cNAMES = []
 cGPAS = []
-
+report_condition = False  
+table = ""
 
 st.image(
     "https://media-hosting.imagekit.io/08168422c2f645f9/IMG_1657.png?Expires=1839961164&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=D51gbtOWwAKHNmzhT6r9HNI5K-VKbdti3-2kbUwdd2A649TiXaLzf6GmwO42h5L8ty1RMsmuEkfIxoADqIWfQVDg0XY0uICM~JgnPdXAA0gIqDZUuVYHXG2O3MWt9GoVGR58Sw71K7H3u3zZ5pEY7ezwOnoXcoLK4oSX1uS~EKvwALG-1K9VTUUBtdsmhFlp~TxkSIOh9TW6jC5FD6~XZodWv1uTvHU7OvVewGnrSrL56RlBtsa0CTaHjkq6tPcq-XYirpcVOUagFam2yWQy23IFAgSdH73tSvhrrkkv6v6qTkkVpt5ZK8qghM2hArQwqG8k~D6z-Kg95D0eIuUaaA__",
@@ -106,6 +106,7 @@ elif ChoGPA == "Unweighted GPA":
       
 try:
   st.warning(f"Your total GPA is {TotGPA/num_classes}")
+  #report_condition = True
 except:
   st.badge("No classes to calculate GPA", color="red")
 
@@ -121,9 +122,35 @@ if booGo == False:
   pass
 elif booGo == True:
   st.table(finTable)
+  report_condition = True
 #pdb.set_trace()
+try:
+  finTable = pd.DataFrame(finTable)
+  csv = finTable.to_csv(index=False).encode('utf-8')
+except:
+  pass
+#buttonDown = st.button()
+if (report_condition == True):
+  if st.button("Prepare Download"):
+  
+    if (report_condition == True):
+      st.download_button(
+          label="Download GPA Report",
+          data = csv,
+          file_name="GPA_Report.csv",
+          mime="Report/csv",
+          icon=":material/download:",
+          type="primary",
+          on_click="ignore"
+      )
+
+  
+  
     
 
     
+
+
+
 
 
