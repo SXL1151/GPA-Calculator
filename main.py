708667f3a409c9175e7d71e25b2f2c5ac3d607ba
SXL1151 <sxl1151@g.coppellisd.com>
2025-11-14 14:45:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ cNAMES = []
 cGPAS = []
 report_condition = False  
 table = ""
-st.sidebar()
 st.sidebar.badge("hello world")
 st.title("GPA CALCULATOR")
 stuVerify = st.checkbox("I am a Coppell ISD student")
