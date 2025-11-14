af502883b3bde5730cdc27f11c95f03fa74aa0d8
SXL1151 <sxl1151@g.coppellisd.com>
2025-11-14 14:22:09 -0600

    Update classAverage.py

--- classAverage.py
+++ classAverage.py
@@ -4,7 +4,7 @@ class AvgInp:
     classAVG = 0
     for i in range(1):
     
-      classAVG = st.number_input("What is your class average %?",
+      classAVG = st.number_input("What is your average(%) in the class?",
            placeholder="Must be a number",min_value=0.10, max_value=100.00,
            key=f"cavgOL_{j}")
     return classAVG
@@ -12,7 +12,7 @@ class AvgInp:
     classAVG = 0
     for i in range(1):
       
-      classAVG = st.number_input("What is your class average %?",
+      classAVG = st.number_input("What is your average(%) in the class?",
            placeholder="Must be a number",min_value=0.10, max_value=100.00,
            key=f"cavgHon_{j}")
     return classAVG
@@ -20,7 +20,7 @@ class AvgInp:
     classAVG = 0
     for i in range(1):
     
-      classAVG = st.number_input("What is your class average %?",
+      classAVG = st.number_input("What is your average(%) in the class?",
          placeholder="Must be a number",min_value=0.10, max_value=100.00,
          key=f"cavgAP_{j}")
     return classAVG
