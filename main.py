aea6d36ced6d80f139663100bf316a197a817173
SXL1151 <sxl1151@g.coppellisd.com>
2025-11-17 12:11:08 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ cGPAS = []
 report_condition = False  
 table = ""
 col1, col2, col3 = st.columns(3)
-with col2:
+with col2 and col3:
   st.title("GPA CALCULATOR")
 stuVerify = st.checkbox("I am a Coppell ISD student")
 if stuVerify == True:
