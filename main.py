9dd32f23fd06d6029aea53157f126a520776d47c
SXL1151 <sxl1151@g.coppellisd.com>
2025-11-18 12:28:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ TotGPA = 0
 cNAMES = []
 cGPAS = []
 def page_1():
+  st.title("")
+def page_2():
   st.title("In Progress")
 report_condition = False  
 table = ""
 st.sidebar.text("Menu")
-nv = st.navigation([page_1])
+nv = st.navigation([page_1,page_2])
 nv.run()
 st.title("GPA CALCULATOR")
 stuVerify = st.checkbox("I am a Coppell ISD student")
