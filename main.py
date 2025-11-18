e96c58c7c5266c20f58467eac9ef6e51c81ade21
SXL1151 <sxl1151@g.coppellisd.com>
2025-11-18 13:07:26 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,18 +6,21 @@ import calc
 TotGPA = 0
 cNAMES = []
 cGPAS = []
-if "page" not in st.session_state:
-  st.session_state.page = ""
-def GPA_Calculator():
-  st.session_state.page = calc.GPACalc()
-  return st.session_state.page
-def page_2():
-  st.title("In Progress")
-report_condition = False  
-table = ""
-st.sidebar.text("Menu")
-nv = st.navigation([GPA_Calculator,page_2])
-nv.run()
+try:
+  if "page" not in st.session_state:
+    st.session_state.page = ""
+  def GPA_Calculator():
+    st.session_state.page = calc.GPACalc()
+    return st.session_state.page
+  def page_2():
+    st.title("In Progress")
+  report_condition = False  
+  table = ""
+  st.sidebar.text("Menu")
+  nv = st.navigation([GPA_Calculator,page_2])
+  nv.run()
+except:
+  pass
   
     
     
